abd5b4d5a88ef072c180d319a957311b7593ab1b
Raymond Alick <raymondalick@gmail.com>
2019-11-04 18:56:19 -0500

    problem2

--- raymond_alick/problem2.py
+++ raymond_alick/problem2.py
@@ -4,15 +4,26 @@ except ImportError:
     print ('Error! Importing problem1.py failed')
     exit()
 
+import datetime
 
-# print ('Execution Time: ', time.time()- start ,' Seconds')
 print(orders_df.groupby('Sales'))
 
-# trends_df = orders_df.groupby('Sales').filter(lambda x: sum(x['var']) > 2000000)
-trends_df = orders_df.groupby('Sales').filter(lambda x: sum(x['var']) > 2000000)
+print(type(orders_df['Order.Date']))
 
-# Group by animal category
-trends_df = orders_df.groupby("Sales")
-# Apply mean function to wieght column
-# animal_groups['weight'].mean()
-print(trends_df)
+orders_df['Month'] = orders_df['Order.Date'].apply(lambda x: datetime.datetime.strptime(x, "%m/%d/%y").strftime('%B'))
+# print(orders_df['month'])
+# print(orders_df)
+# trends_df = orders_df.groupby(['Month','Category']).['Month'].sort()
+category_trends_df = orders_df.groupby(['Month','Category'])['Quantity'].count()
+# category_trends_df = orders_df.groupby(['Month','Category'])['Quantity'].count()
+
+# category_trends_df = category_trends_df.assign(sales = lambda x: np.mean(x.sales))
+# category_trends_df = category_trends_df.apply(sales = lambda x: np.mean(x.sales))
+
+print(category_trends_df)
+
+
+
+
+
+# print ('Execution Time: ', time.time()- start ,' Seconds')
